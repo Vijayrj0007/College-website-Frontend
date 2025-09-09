59c64423c189afe1af56a933153702e88d375261
vijay kumar Pandit <cutevijay0007@gmail.com>
2025-09-10 00:03:11 +0530

    f

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -4,7 +4,7 @@ import axios from "axios";
 const BASE_URL =
   import.meta.env.VITE_API_URL || 
   (import.meta.env.PROD 
-    ? "https://college-website-iota-jet.vercel.app/" 
+    ? "https://college-website-iota-jet.vercel.app/api" 
     : "http://localhost:5000/api");
 
 const api = axios.create({
